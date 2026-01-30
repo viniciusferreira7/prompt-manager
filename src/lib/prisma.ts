34ebcc983ac7f2fed4f747e4c45220dfed0535f0
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-30 09:07:22 -0300

    chore: add envs into prisma config

---
+++ src/lib/prisma.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from '@/generated/prisma/client';
+import { env } from './env';
+
+export const prismaClient = new PrismaClient({
+  log: env.NODE_ENV !== 'production' ? ['error', 'query', 'warn', 'info'] : ['info'],
+  accelerateUrl: env.DATABASE_URL
+})
